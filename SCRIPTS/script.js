6e11d915b5aa94960bfe20f4402d2022f0746756
unknown user <unknownpng007@gmail.com>
2024-01-25 14:06:50 +0545

    commit #2

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -1,20 +1,45 @@
- document.addEventListener("DOMContentLoaded", function() {
-            // Hide the skeleton and show the images when they are loaded
-            var images = document.querySelectorAll('.scrollable-images img');
-            var skeleton = document.querySelector('.skeleton-container');
+const track = document.getElementById("scrools);
 
-            images.forEach(function(image) {
-                image.addEventListener('load', function() {
-                    skeleton.style.display = 'none';
-                    image.style.display = 'block';
-                });
-            });
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
-            // Automatically change images every 3 seconds (adjust as needed)
-            var currentIndex = 0;
-            setInterval(function() {
-                images[currentIndex].style.display = 'none';
-                currentIndex = (currentIndex + 1) % images.length;
-                images[currentIndex].style.display = 'block';
-            }, 3000);
-        });
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
